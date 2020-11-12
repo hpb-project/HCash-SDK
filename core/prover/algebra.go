df3e0da213b308c5fd979d07e5f15072a6f41291
rjgeek <jiaxuan.li@hpb.io>
2020-11-13 00:03:35 +0800

    finish algebra.go

--- core/prover/algebra.go
+++ core/prover/algebra.go
@@ -2,7 +2,7 @@ package prover
 
 import (
 	"encoding/hex"
-	"github.com/ethereum/go-ethereum/accounts/abi"
+	//"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/hpb-project/HCash-SDK/core/ebigint"
 	"github.com/hpb-project/HCash-SDK/core/utils"
 	"github.com/hpb-project/HCash-SDK/core/utils/bn128"
@@ -55,8 +55,25 @@ func (g GeneratorParams) GetHS() *GeneratorVector {
 	return g.hs
 }
 
-func (g *GeneratorParams) Commit(blinding, gExp, hExp string) {
-	// todo: implement commit.
+func (g *GeneratorParams) Commit(blinding *ebigint.NBigInt, gExp, hExp *FieldVector) utils.Point {
+	b128 := utils.NewBN128()
+	var result = b128.G1.MulScalar(g.h, blinding.Int)
+	var gsVector = g.gs.GetVector()
+	gexpVector := gExp.GetVector()
+	for i, gexp := range gexpVector {
+		t := b128.G1.MulScalar(gsVector[i], gexp.Int)
+		result = b128.G1.Add(result, t)
+	}
+
+	if hExp != nil {
+		var hsVector = g.hs.GetVector()
+		hexpVector := hExp.GetVector()
+		for i, hexp := range hexpVector {
+			t := b128.G1.MulScalar(hsVector[i], hexp.Int)
+			result = b128.G1.Add(result, t)
+		}
+	}
+	return result
 }
 
 type FieldVector struct {
@@ -70,11 +87,11 @@ func NewFieldVector(vector []*ebigint.NBigInt) *FieldVector {
 	return fv
 }
 
-func (f *FieldVector)GetVector() []*ebigint.NBigInt {
+func (f *FieldVector) GetVector() []*ebigint.NBigInt {
 	return f.vector
 }
 
-func (f *FieldVector)Length() int {
+func (f *FieldVector) Length() int {
 	return len(f.vector)
 }
 
@@ -87,37 +104,36 @@ func (f *FieldVector) Add(other *FieldVector) *FieldVector {
 	fq := bn128.NewFq(f.vector[0].GetRed().Number())
 	var innards = other.GetVector()
 	var nInnards = make([]*ebigint.NBigInt, len(f.vector))
-	for i,accum := range f.vector {
+
+	for i, accum := range f.vector {
 		nInnards[i] = ebigint.ToNBigInt(fq.Add(accum.Int, innards[i].Int))
 	}
 
 	return NewFieldVector(nInnards)
 }
 
-
-func (f *FieldVector) Plus (constant *ebigint.NBigInt) *FieldVector {
+func (f *FieldVector) Plus(constant *ebigint.NBigInt) *FieldVector {
 	fq := bn128.NewFq(f.vector[0].GetRed().Number())
 	var nInnards = make([]*ebigint.NBigInt, len(f.vector))
-	for i,accum := range f.vector {
+	for i, accum := range f.vector {
 		nInnards[i] = ebigint.ToNBigInt(fq.Add(accum.Int, constant.Int))
 	}
 
 	return NewFieldVector(nInnards)
 }
 
-func (f *FieldVector) Sum () *ebigint.NBigInt {
+func (f *FieldVector) Sum() *ebigint.NBigInt {
 	b128 := utils.NewBN128()
-	fq := bn128.NewFq(f.vector[0].GetRed().Number())
+	fq := bn128.NewFq(b128.Q().Number())
 	var nVectors = make([]*ebigint.NBigInt, 0)
 	nVectors = append(nVectors, ebigint.ToNBigInt(big.NewInt(0)).ToRed(b128.Q()))
-	for _,c := range f.vector {
+	for _, c := range f.vector {
 		nVectors = append(nVectors, c)
 	}
 
-	// todo : replace to GetVector()
-	var current *ebigint.NBigInt
+	var current = ebigint.ToNBigInt(big.NewInt(0)).ToRed(b128.Q())
 
-	for _,accum := range nVectors {
+	for _, accum := range nVectors {
 		current = ebigint.ToNBigInt(fq.Add(accum.Int, current.Int))
 	}
 
@@ -127,7 +143,7 @@ func (f *FieldVector) Sum () *ebigint.NBigInt {
 func (f *FieldVector) Negate() *FieldVector {
 	fq := bn128.NewFq(f.vector[0].GetRed().Number())
 	var nInnards = make([]*ebigint.NBigInt, len(f.vector))
-	for i,accum := range f.vector {
+	for i, accum := range f.vector {
 		nInnards[i] = ebigint.ToNBigInt(fq.Neg(accum.Int))
 	}
 
@@ -142,7 +158,7 @@ func (f *FieldVector) Hadamard(other *FieldVector) *FieldVector {
 	fq := bn128.NewFq(f.vector[0].GetRed().Number())
 	var innards = other.vector
 	var nInnards = make([]*ebigint.NBigInt, len(f.vector))
-	for i,accum := range f.vector {
+	for i, accum := range f.vector {
 		nInnards[i] = ebigint.ToNBigInt(fq.Mul(accum.Int, innards[i].Int))
 	}
 
@@ -152,7 +168,7 @@ func (f *FieldVector) Hadamard(other *FieldVector) *FieldVector {
 func (f *FieldVector) Invert() *FieldVector {
 	fq := bn128.NewFq(f.vector[0].GetRed().Number())
 	var nInnards = make([]*ebigint.NBigInt, len(f.vector))
-	for i,accum := range f.vector {
+	for i, accum := range f.vector {
 		nInnards[i] = ebigint.ToNBigInt(fq.Inverse(accum.Int))
 	}
 
@@ -161,7 +177,7 @@ func (f *FieldVector) Invert() *FieldVector {
 
 func (f *FieldVector) Extract(parity int) *FieldVector {
 	var nInnards = make([]*ebigint.NBigInt, 0)
-	for i,accum := range f.vector {
+	for i, accum := range f.vector {
 		if i%2 == parity {
 			nInnards = append(nInnards, accum)
 		}
@@ -172,7 +188,7 @@ func (f *FieldVector) Extract(parity int) *FieldVector {
 func (f *FieldVector) Flip() *FieldVector {
 	var size = len(f.vector)
 	var nInnards = make([]*ebigint.NBigInt, size)
-	for i,_ := range f.vector {
+	for i, _ := range f.vector {
 		nInnards[i] = f.vector[(size-i)%size]
 	}
 
@@ -181,11 +197,11 @@ func (f *FieldVector) Flip() *FieldVector {
 
 func (f *FieldVector) Concat(other *FieldVector) *FieldVector {
 	var nInnards = make([]*ebigint.NBigInt, 0)
-	for _,elem := range f.vector {
+	for _, elem := range f.vector {
 		nInnards = append(nInnards, elem)
 	}
 
-	for _,elem := range other.vector {
+	for _, elem := range other.vector {
 		nInnards = append(nInnards, elem)
 	}
 
@@ -195,7 +211,7 @@ func (f *FieldVector) Concat(other *FieldVector) *FieldVector {
 func (f *FieldVector) Times(constant *ebigint.NBigInt) *FieldVector {
 	fq := bn128.NewFq(f.vector[0].GetRed().Number())
 	var nInnards = make([]*ebigint.NBigInt, len(f.vector))
-	for i,accum := range f.vector {
+	for i, accum := range f.vector {
 		nInnards[i] = ebigint.ToNBigInt(fq.Mul(accum.Int, constant.Int))
 	}
 
@@ -206,18 +222,17 @@ func (f *FieldVector) InnerProduct(other *FieldVector) *ebigint.NBigInt {
 	var innards = other.GetVector()
 
 	b128 := utils.NewBN128()
-	fq := bn128.NewFq(f.vector[0].GetRed().Number())
+	fq := bn128.NewFq(b128.Q().Number())
 	var nVectors = make([]*ebigint.NBigInt, 0)
 	nVectors = append(nVectors, ebigint.ToNBigInt(big.NewInt(0)).ToRed(b128.Q()))
-	for _,c := range f.vector {
+	for _, c := range f.vector {
 		nVectors = append(nVectors, c)
 	}
 
-	// todo : replace to GetVector()
-	var current *ebigint.NBigInt
+	var current = ebigint.ToNBigInt(big.NewInt(0)).ToRed(b128.Q())
 
-	for i,accum := range nVectors {
-		t := fq.Mul(current.Int,innards[i].Int)
+	for i, accum := range nVectors {
+		t := fq.Mul(current.Int, innards[i].Int)
 		current = ebigint.ToNBigInt(fq.Add(accum.Int, t))
 	}
 
@@ -243,26 +258,24 @@ func (g *GeneratorVector) Length() int {
 	return len(g.vector)
 }
 
-func (g *GeneratorVector) Slice(begin,end int) *GeneratorVector {
+func (g *GeneratorVector) Slice(begin, end int) *GeneratorVector {
 	return NewGeneratorVector(g.vector[begin:end])
 }
 
-func (g *GeneratorVector) Commit(exponents []*ebigint.NBigInt) utils.Point {
+func (g *GeneratorVector) Commit(exponents *FieldVector) utils.Point {
 	b128 := utils.NewBN128()
 	var nVectors = make([]utils.Point, 0)
 	nVectors = append(nVectors, b128.Zero())
-	for _,c := range g.vector {
+	for _, c := range g.vector {
 		nVectors = append(nVectors, c)
 	}
 
-	// todo : replace to GetVector()
-	var innards = exponents
+	var innards = exponents.GetVector()
+	var current = b128.Zero()
 
-	var current utils.Point
-
-	for i,accum := range nVectors {
+	for i, accum := range nVectors {
 		t := b128.G1.MulScalar(current, innards[i].Int)
-		current = b128.G1.Add(accum,t)
+		current = b128.G1.Add(accum, t)
 	}
 
 	return current
@@ -272,13 +285,13 @@ func (g *GeneratorVector) Sum() utils.Point {
 	b128 := utils.NewBN128()
 	var nVectors = make([]utils.Point, 0)
 	nVectors = append(nVectors, b128.Zero())
-	for _,c := range g.vector {
+	for _, c := range g.vector {
 		nVectors = append(nVectors, c)
 	}
 
-	var current utils.Point
+	var current = b128.Zero()
 
-	for _,accum := range nVectors {
+	for _, accum := range nVectors {
 		current = b128.G1.Add(accum, current)
 	}
 
@@ -289,18 +302,18 @@ func (g *GeneratorVector) Add(other *GeneratorVector) *GeneratorVector {
 	b128 := utils.NewBN128()
 	var innards = other.GetVector()
 	var nInnards = make([]utils.Point, len(g.vector))
-	for i,accum := range g.vector {
+	for i, accum := range g.vector {
 		nInnards[i] = b128.G1.Add(accum, innards[i])
 	}
 
 	return NewGeneratorVector(nInnards)
 }
 
-func (g *GeneratorVector) Hadamard(exponents []*ebigint.NBigInt) *GeneratorVector {
+func (g *GeneratorVector) Hadamard(exponents *FieldVector) *GeneratorVector {
 	b128 := utils.NewBN128()
-	var innards = exponents
+	var innards = exponents.GetVector()
 	var nInnards = make([]utils.Point, len(g.vector))
-	for i,elem := range g.vector {
+	for i, elem := range g.vector {
 		nInnards[i] = b128.G1.MulScalar(elem, innards[i].Int)
 	}
 
@@ -310,17 +323,17 @@ func (g *GeneratorVector) Hadamard(exponents []*ebigint.NBigInt) *GeneratorVecto
 func (g *GeneratorVector) Negate() *GeneratorVector {
 	b128 := utils.NewBN128()
 	var nInnards = make([]utils.Point, len(g.vector))
-	for i,elem := range g.vector {
+	for i, elem := range g.vector {
 		nInnards[i] = b128.G1.Neg(elem)
 	}
 
 	return NewGeneratorVector(nInnards)
 }
 
-func (g *GeneratorVector) Times(constant *ebigint.NBigInt) *GeneratorVector{
+func (g *GeneratorVector) Times(constant *ebigint.NBigInt) *GeneratorVector {
 	b128 := utils.NewBN128()
 	var nInnards = make([]utils.Point, len(g.vector))
-	for i,elem := range g.vector {
+	for i, elem := range g.vector {
 		nInnards[i] = b128.G1.MulScalar(elem, constant.Int)
 	}
 
@@ -329,7 +342,7 @@ func (g *GeneratorVector) Times(constant *ebigint.NBigInt) *GeneratorVector{
 
 func (g *GeneratorVector) Extract(parity int) *GeneratorVector {
 	var nInnards = make([]utils.Point, len(g.vector))
-	for i,elem := range g.vector {
+	for i, elem := range g.vector {
 		if i%2 == parity {
 			nInnards = append(nInnards, elem)
 		}
@@ -338,9 +351,9 @@ func (g *GeneratorVector) Extract(parity int) *GeneratorVector {
 	return NewGeneratorVector(nInnards)
 }
 
-func (g *GeneratorVector) Concat(other *GeneratorVector) *GeneratorVector{
+func (g *GeneratorVector) Concat(other *GeneratorVector) *GeneratorVector {
 	var nInnards = make([]utils.Point, 0)
-	for _,elem := range g.vector {
+	for _, elem := range g.vector {
 		nInnards = append(nInnards, elem)
 	}
 
@@ -365,45 +378,95 @@ func NewConvolver() *Convolver {
 	return c
 }
 
-func (c *Convolver) FFT(input []string, inverse bool) []string {
-	var length = len(input)
+//
+//type Something interface {
+//	Length() int
+//	Extract(int) Something
+//}
+
+func (c *Convolver) FFT_Scalar(input *FieldVector, inverse bool) *FieldVector {
+	var length = input.Length()
 	if length == 1 {
 		return input
 	}
 	if length%2 != 0 {
 		panic("Input size must be a power of 2!")
 	}
+	b128 := utils.NewBN128()
 	fq := bn128.NewFq(c.unity.GetRed().Number())
-	base := big.NewInt(1).Lsh(big.NewInt(1), 28)
-	var omega = fq.Exp(c.unity.Int, base.Div(base, big.NewInt(int64(length))))
+	exp := big.NewInt(1).Lsh(big.NewInt(1), 28)
+	var omega = fq.Exp(c.unity.Int, exp.Div(exp, big.NewInt(int64(length))))
 	if inverse {
 		omega = fq.Inverse(omega)
 	}
-	var even = c.FFT(input.extract(0), inverse)
-	var odd  = c.FFT(input.extract(1), inverse)
+	var even = c.FFT_Scalar(input.Extract(0), inverse)
+	var odd = c.FFT_Scalar(input.Extract(1), inverse)
 
 	var omegas = make([]*ebigint.NBigInt, 0)
+
 	omegas = append(omegas, ebigint.ToNBigInt(big.NewInt(1)).ToRed(c.unity.GetRed()))
 
-	for i:=0; i < length/2; i++ {
-		omegas = append(omegas, ebigint.ToNBigInt(fq.Mul(omegas[i-1].Int, omega)))
+	for i := 1; i < length/2; i++ {
+		t := fq.Mul(omegas[i-1].Int, omega)
+		omegas = append(omegas, ebigint.ToNBigInt(t).ToRed(c.unity.GetRed()))
 	}
 
-	// todo : implement this.
 	var nomegas = NewFieldVector(omegas)
-	var result = even.Add(odd.Hadamard(omegas)).Concat(even.Add(odd.Hadamard(omegas).Negate()))
+	var result = even.Add(odd.Hadamard(nomegas)).Concat(even.Add(odd.Hadamard(nomegas).Negate()))
 	if inverse {
-		result = result.times(new BN(2).toRed(bn128.q).redInvm());
+		t := ebigint.ToNBigInt(big.NewInt(2)).ToRed(b128.Q())
+		t = ebigint.ToNBigInt(fq.Inverse(t.Int)).ToRed(b128.Q())
+		result = result.Times(t)
 	}
 	return result
 }
 
-func (c *Convolver) Convolution(exponent int, base []string) {
-	size := len(base)
-	temp := c.FFT(base, false).Hadamard(c.FFT(exponent.Flip(), false))
+func (c *Convolver) Convolution_Scalar(exponent *FieldVector, base *FieldVector) *FieldVector {
+	b128 := utils.NewBN128()
+	fq := bn128.NewFq(b128.Q().Number())
+	size := base.Length()
+	temp := c.FFT_Scalar(base, false).Hadamard(c.FFT_Scalar(exponent.Flip(), false))
 
-	return fft(temp.slice(0, size / 2).add(temp.slice(size / 2)).times(new BN(2).toRed(bn128.q).redInvm()), true);
+	t := ebigint.ToNBigInt(big.NewInt(2)).ToRed(b128.Q())
+	t = ebigint.ToNBigInt(fq.Inverse(t.Int)).ToRed(b128.Q())
+	return c.FFT_Scalar(temp.Slice(0, size/2).Add(temp.Slice(size/2, size)).Times(t), true)
+}
 
+func (c *Convolver) FFT_Point(input *GeneratorVector, inverse bool) *GeneratorVector {
+	var length = input.Length()
+	if length == 1 {
+		return input
+	}
+	if length%2 != 0 {
+		panic("Input size must be a power of 2!")
+	}
+	b128 := utils.NewBN128()
+	fq := bn128.NewFq(c.unity.GetRed().Number())
+	exp := big.NewInt(1).Lsh(big.NewInt(1), 28)
+	var omega = fq.Exp(c.unity.Int, exp.Div(exp, big.NewInt(int64(length))))
+	if inverse {
+		omega = fq.Inverse(omega)
+	}
+	var even = c.FFT_Point(input.Extract(0), inverse)
+	var odd = c.FFT_Point(input.Extract(1), inverse)
+
+	var omegas = make([]*ebigint.NBigInt, 0)
+
+	omegas = append(omegas, ebigint.ToNBigInt(big.NewInt(1)).ToRed(c.unity.GetRed()))
+
+	for i := 1; i < length/2; i++ {
+		t := fq.Mul(omegas[i-1].Int, omega)
+		omegas = append(omegas, ebigint.ToNBigInt(t).ToRed(c.unity.GetRed()))
+	}
+
+	var nomegas = NewFieldVector(omegas)
+	var result = even.Add(odd.Hadamard(nomegas)).Concat(even.Add(odd.Hadamard(nomegas).Negate()))
+	if inverse {
+		t := ebigint.ToNBigInt(big.NewInt(2)).ToRed(b128.Q())
+		t = ebigint.ToNBigInt(fq.Inverse(t.Int)).ToRed(b128.Q())
+		result = result.Times(t)
+	}
+	return result
 }
 
 type FieldVectorPolynomial struct {
@@ -427,7 +490,7 @@ func (f *FieldVectorPolynomial) Evaluate(x *ebigint.NBigInt) *PedersenCommitment
 	fq := bn128.NewFq(x.GetRed().Number())
 	for _, coefficient := range f.coefficients[1:] {
 		result.Add(coefficient.Times(accumulator))
-		accumulator = ebigint.ToNBigInt(fq.Mul(accumulator.Int, x.Int))
+		accumulator = ebigint.ToNBigInt(fq.Mul(accumulator.Int, x.Int)).ToRed(x.GetRed())
 	}
 
 	return result
@@ -448,6 +511,7 @@ func (f *FieldVectorPolynomial) InnerProduct(other *FieldVectorPolynomial) []*eb
 		mine := f.coefficients[i]
 		for j := 0; j < len(innards); j++ {
 			theirs := innards[j]
+			// todo: check mine type.
 			result[i+j] = ebigint.ToNBigInt(fq.Add(result[i+j].Int, mine.InnerProduct(theirs)))
 		}
 	}
@@ -480,19 +544,20 @@ func (pc PedersenCommitment) GetR() *ebigint.NBigInt {
 }
 
 func (pc PedersenCommitment) Commit() utils.Point {
-	bn128 := utils.NewBN128()
-	t1 := bn128.G1.MulScalar(pc.params.GetG(), pc.x.Int)
-	t2 := bn128.G1.MulScalar(pc.params.GetH(), pc.r.Int)
-	result := bn128.G1.Add(t1, t2)
+	b128 := utils.NewBN128()
+	t1 := b128.G1.MulScalar(pc.params.GetG(), pc.x.Int)
+	t2 := b128.G1.MulScalar(pc.params.GetH(), pc.r.Int)
+	result := b128.G1.Add(t1, t2)
 
-	return utils.Point(result)
+	return result
 }
 
 func (pc PedersenCommitment) Add(other *PedersenCommitment) *PedersenCommitment {
 	fq := bn128.NewFq(pc.x.GetRed().Number())
 	nx := fq.Add(pc.x.Int, other.GetX().Int)
 	nr := fq.Add(pc.r.Int, other.GetR().Int)
-	result := NewPedersenCommitment(pc.params, ebigint.ToNBigInt(nx), ebigint.ToNBigInt(nr))
+	result := NewPedersenCommitment(pc.params, ebigint.ToNBigInt(nx).ToRed(other.GetX().GetRed()),
+		ebigint.ToNBigInt(nr).ToRed(other.GetR().GetRed()))
 
 	return result
 }
@@ -501,27 +566,26 @@ func (pc PedersenCommitment) Times(exponent *ebigint.NBigInt) *PedersenCommitmen
 	fq := bn128.NewFq(pc.x.GetRed().Number())
 	nx := fq.Mul(pc.x.Int, exponent.Int)
 	nr := fq.Mul(pc.r.Int, exponent.Int)
-	result := NewPedersenCommitment(pc.params, ebigint.ToNBigInt(nx), ebigint.ToNBigInt(nr))
+	result := NewPedersenCommitment(pc.params, ebigint.ToNBigInt(nx).ToRed(pc.x.GetRed()),
+		ebigint.ToNBigInt(nr).ToRed(pc.x.GetRed()))
 
 	return result
 }
 
 type PolyCommitment struct {
-	params                 GeneratorParams
 	coefficientCommitments []*PedersenCommitment
 }
 
 func NewPolyCommitment(params GeneratorParams, coefficients []*ebigint.NBigInt) *PolyCommitment {
-	bn128 := utils.NewBN128()
+	b128 := utils.NewBN128()
 
 	pc := &PolyCommitment{}
-	pc.params = params
 	pc.coefficientCommitments = make([]*PedersenCommitment, 0)
-	tmp := NewPedersenCommitment(params, coefficients[0], ebigint.ToNBigInt(big.NewInt(0)).ToRed(bn128.Q()))
+	tmp := NewPedersenCommitment(params, coefficients[0], ebigint.ToNBigInt(big.NewInt(0)).ToRed(b128.Q()))
 	pc.coefficientCommitments = append(pc.coefficientCommitments, tmp)
 
 	for _, coefficient := range coefficients[1:] {
-		rand, _ := bn128.RanddomScalar()
+		rand, _ := b128.RanddomScalar()
 		npc := NewPedersenCommitment(params, coefficient, rand)
 		pc.coefficientCommitments = append(pc.coefficientCommitments, npc)
 	}
@@ -544,7 +608,7 @@ func (pc *PolyCommitment) Evaluate(x *ebigint.NBigInt) *PedersenCommitment {
 	fq := bn128.NewFq(x.GetRed().Number())
 	for _, commitment := range pc.coefficientCommitments[1:] {
 		result = result.Add(commitment.Times(accumulator))
-		accumulator = ebigint.ToNBigInt(fq.Mul(accumulator.Int, x.Int))
+		accumulator = ebigint.ToNBigInt(fq.Mul(accumulator.Int, x.Int)).ToRed(x.GetRed())
 	}
 	return result
 }
@@ -558,22 +622,24 @@ func NewPolynomial(coefficients []*ebigint.NBigInt) *Polynomial {
 	if coefficients != nil && len(coefficients) > 0 {
 		poly.coefficients = coefficients
 	} else {
-		bn128 := utils.NewBN128()
-		p := ebigint.ToNBigInt(big.NewInt(1)).ToRed(bn128.Q())
-		poly.coefficients = []*ebigint.NBigInt{p}
+		poly.coefficients = make([]*ebigint.NBigInt, 0)
+		b128 := utils.NewBN128()
+		p := ebigint.ToNBigInt(big.NewInt(1)).ToRed(b128.Q())
+		poly.coefficients = append(poly.coefficients, p)
 	}
 
 	return poly
 }
 
 func (p *Polynomial) Mul(other *Polynomial) *Polynomial {
-	fq := bn128.NewFq(utils.NewBN128().Q().Number())
+	b128 := utils.NewBN128()
+	fq := bn128.NewFq(b128.Q().Number())
 	product := make([]*ebigint.NBigInt, len(p.coefficients))
 	for i, b := range p.coefficients {
-		product[i] = ebigint.ToNBigInt(fq.Mul(b.Int, other.coefficients[0].Int))
+		product[i] = ebigint.ToNBigInt(fq.Mul(b.Int, other.coefficients[0].Int)).ToRed(b128.Q())
 	}
 
-	product = append(product, ebigint.ToNBigInt(big.NewInt(0)).ToRed(utils.NewBN128().Q()))
+	product = append(product, ebigint.ToNBigInt(big.NewInt(0)).ToRed(b128.Q()))
 
 	if other.coefficients[1].Cmp(big.NewInt(1)) == 0 {
 		// product = product.map((product_i, i) => i > 0 ? product_i.redAdd(this.coefficients[i - 1]) : product_i);
@@ -581,7 +647,7 @@ func (p *Polynomial) Mul(other *Polynomial) *Polynomial {
 		nproduct := make([]*ebigint.NBigInt, len(product))
 		for i, b := range product {
 			if i > 0 {
-				nproduct[i] = ebigint.ToNBigInt(fq.Add(b.Int, product[i-1].Int))
+				nproduct[i] = ebigint.ToNBigInt(fq.Add(b.Int, product[i-1].Int)).ToRed(b128.Q())
 			} else {
 				nproduct[i] = b
 			}
