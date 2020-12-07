bec31bbd472bb5144065e79cb10b46633261db9e
rjgeek <jiaxuan.li@hpb.io>
2020-12-07 15:10:16 +0800

    update zether.go

--- core/prover/zether.go
+++ core/prover/zether.go
@@ -39,7 +39,7 @@ type ZetherProof struct {
 	s_b   *ebigint.NBigInt
 	s_tau *ebigint.NBigInt
 
-	ipProof InnerProductProof
+	ipProof *InnerProductProof
 }
 
 func (z ZetherProof) Serialize() string {
@@ -595,241 +595,374 @@ func (this ZetherProver) GenerateProof(statement map[string]interface{}, witness
 			w = utils.Hash(hex.EncodeToString(bytes))
 		}
 	}
-		proof.f = b.Times(w).Add(a)
-		{
-			a1 := fq.Mul(r_B.Int, w.Int)
-			a2 := fq.Add(a1, r_A.Int)
-			proof.z_A = ebigint.ToNBigInt(a2).ToRed(w.GetRed())
+	proof.f = b.Times(w).Add(a)
+	{
+		a1 := fq.Mul(r_B.Int, w.Int)
+		a2 := fq.Add(a1, r_A.Int)
+		proof.z_A = ebigint.ToNBigInt(a2).ToRed(w.GetRed())
+	}
+	var y *ebigint.NBigInt
+	{
+		arguments = abi.Arguments{
+			{
+				Type: bytes32_T,
+			},
 		}
-		var y *ebigint.NBigInt
-		{
-			arguments = abi.Arguments{
-				{
-					Type: bytes32_T,
-				},
-			}
 
-			bytes, _ = arguments.Pack(
-				b128.Bytes(w.Int),
-			)
-			y = utils.Hash(hex.EncodeToString(bytes))
+		bytes, _ = arguments.Pack(
+			b128.Bytes(w.Int),
+		)
+		y = utils.Hash(hex.EncodeToString(bytes))
+	}
+	var vys = make([]*ebigint.NBigInt, 0)
+	{
+		vys = append(vys, ebigint.NewNBigInt(1).ToRed(b128.Q()))
+		for i := 1; i < 64; i++ {
+			p := vys[i-1]
+			nv := ebigint.ToNBigInt(fq.Mul(p.Int, y.Int)).ToRed(b128.Q())
+			vys = append(vys, nv)
 		}
-		var vys = make([]*ebigint.NBigInt, 0)
-		{
-			vys = append(vys, ebigint.NewNBigInt(1).ToRed(b128.Q()))
-			for i := 1; i < 64; i++ {
-				p := vys[i-1]
-				nv := ebigint.ToNBigInt(fq.Mul(p.Int, y.Int)).ToRed(b128.Q())
-				vys = append(vys, nv)
-			}
-		}
-		ys := NewFieldVector(vys)
-		z := utils.Hash(b128.Bytes(y.Int))
-		zs := make([]*ebigint.NBigInt, 0)
-		{
-			zs = append(zs, ebigint.ToNBigInt(fq.Exp(z.Int,big.NewInt(2))).ToRed(z.GetRed()))
-			zs = append(zs, ebigint.ToNBigInt(fq.Exp(z.Int,big.NewInt(3))).ToRed(z.GetRed()))
-		}
-		var twos = make([]*ebigint.NBigInt, 0)
-		var v_twoTimesZs = make([]*ebigint.NBigInt,0)
-		{
-			twos = append(twos, ebigint.NewNBigInt(1).ToRed(b128.Q()))
-			for i:=1; i < 32; i++ {
-				a1 := twos[i-1]
-				a2 := big.NewInt(2)
+	}
+	ys := NewFieldVector(vys)
+	z := utils.Hash(b128.Bytes(y.Int))
+	zs := make([]*ebigint.NBigInt, 0)
+	{
+		zs = append(zs, ebigint.ToNBigInt(fq.Exp(z.Int, big.NewInt(2))).ToRed(z.GetRed()))
+		zs = append(zs, ebigint.ToNBigInt(fq.Exp(z.Int, big.NewInt(3))).ToRed(z.GetRed()))
+	}
+	var twos = make([]*ebigint.NBigInt, 0)
+	var v_twoTimesZs = make([]*ebigint.NBigInt, 0)
+	{
+		twos = append(twos, ebigint.NewNBigInt(1).ToRed(b128.Q()))
+		for i := 1; i < 32; i++ {
+			a1 := twos[i-1]
+			a2 := big.NewInt(2)
 
-				b := fq.Mul(a1.Int, a2)
-				twos = append(twos, ebigint.ToNBigInt(b).ToRed(a1.GetRed()))
-			}
+			b := fq.Mul(a1.Int, a2)
+			twos = append(twos, ebigint.ToNBigInt(b).ToRed(a1.GetRed()))
+		}
 
-			for i:=0; i < 2; i++ {
-				for j:=0; j < 32; j++ {
-					a1 := zs[i]
-					a2 := twos[j]
-					p  := fq.Mul(a1.Int, a2.Int)
-					v_twoTimesZs = append(v_twoTimesZs, ebigint.ToNBigInt(p).ToRed(a1.GetRed()))
-				}
+		for i := 0; i < 2; i++ {
+			for j := 0; j < 32; j++ {
+				a1 := zs[i]
+				a2 := twos[j]
+				p := fq.Mul(a1.Int, a2.Int)
+				v_twoTimesZs = append(v_twoTimesZs, ebigint.ToNBigInt(p).ToRed(a1.GetRed()))
 			}
 		}
-		twoTimesZs := NewFieldVector(v_twoTimesZs)
-		nz := ebigint.ToNBigInt(fq.Neg(z.Int)).ToRed(z.GetRed())
-		var lPoly = NewFieldVectorPolynomial(aL.Plus(nz), sL)
-		var rPoly = NewFieldVectorPolynomial(ys.Hadamard(aR.Plus(z)).Add(twoTimesZs), sR.Hadamard(ys))
-		var tPolyCoefficients = lPoly.InnerProduct(rPoly)
-		var polyCommitment = NewPolyCommitment(*this.params, tPolyCoefficients)
+	}
+	twoTimesZs := NewFieldVector(v_twoTimesZs)
+	nz := ebigint.ToNBigInt(fq.Neg(z.Int)).ToRed(z.GetRed())
+	var lPoly = NewFieldVectorPolynomial(aL.Plus(nz), sL)
+	var rPoly = NewFieldVectorPolynomial(ys.Hadamard(aR.Plus(z)).Add(twoTimesZs), sR.Hadamard(ys))
+	var tPolyCoefficients = lPoly.InnerProduct(rPoly)
+	var polyCommitment = NewPolyCommitment(*this.params, tPolyCoefficients)
 
-		proof.tCommits = NewGeneratorVector(polyCommitment.GetCommitments())
+	proof.tCommits = NewGeneratorVector(polyCommitment.GetCommitments())
 
-		var x *ebigint.NBigInt
-		{
-			arguments = abi.Arguments{
-				{
-					Type: bytes32_T,
-				},
-				{
-					Type: bytes32_2T,
-				},
-				{
-					Type: bytes32_2T,
-				},
-			}
-			vt := polyCommitment.GetCommitments()
-			vvt := make([][2]string,0)
-			for i:=0; i < len(vt); i++ {
-				x,y := b128.Serialize(vt[i])
-				vvt = append(vvt, [2]string{x,y})
-			}
-			bytes, _ = arguments.Pack(
-				b128.Bytes(z.Int),
-				vvt[0],
-				vvt[1],
-			)
-			x = utils.Hash(hex.EncodeToString(bytes))
+	var x *ebigint.NBigInt
+	{
+		arguments = abi.Arguments{
+			{
+				Type: bytes32_T,
+			},
+			{
+				Type: bytes32_2T,
+			},
+			{
+				Type: bytes32_2T,
+			},
 		}
-		var evalCommit = polyCommitment.Evaluate(x)
-		proof.tHat = evalCommit.GetX()
-		var tauX = evalCommit.GetR()
-		{
-			a1 := fq.Mul(rho.Int, x.Int)
-			proof.mu = ebigint.ToNBigInt(fq.Add(alpha.Int, a1)).ToRed(alpha.GetRed())
+		vt := polyCommitment.GetCommitments()
+		vvt := make([][2]string, 0)
+		for i := 0; i < len(vt); i++ {
+			x, y := b128.Serialize(vt[i])
+			vvt = append(vvt, [2]string{x, y})
 		}
-		var CRnR = b128.Zero()
-		var y_0R = b128.Zero()
-		var y_XR = b128.Zero()
-		var DR   = b128.Zero()
-		var gR   = b128.Zero()
-		var p,q *FieldVector
-		{
-			v_p := make([]*ebigint.NBigInt,N)
-			v_q := make([]*ebigint.NBigInt,N)
-			for i:=0; i < N; i++ {
-				v_p[i] = ebigint.NewNBigInt(0).ToRed(b128.Q())
-				v_q[i] = ebigint.NewNBigInt(0).ToRed(b128.Q())
-			}
-			p = NewFieldVector(v_p)
-			q = NewFieldVector(v_q)
+		bytes, _ = arguments.Pack(
+			b128.Bytes(z.Int),
+			vvt[0],
+			vvt[1],
+		)
+		x = utils.Hash(hex.EncodeToString(bytes))
+	}
+	var evalCommit = polyCommitment.Evaluate(x)
+	proof.tHat = evalCommit.GetX()
+	var tauX = evalCommit.GetR()
+	{
+		a1 := fq.Mul(rho.Int, x.Int)
+		proof.mu = ebigint.ToNBigInt(fq.Add(alpha.Int, a1)).ToRed(alpha.GetRed())
+	}
+	var CRnR = b128.Zero()
+	var y_0R = b128.Zero()
+	var y_XR = b128.Zero()
+	var DR = b128.Zero()
+	var gR = b128.Zero()
+	var p, q *FieldVector
+	{
+		v_p := make([]*ebigint.NBigInt, N)
+		v_q := make([]*ebigint.NBigInt, N)
+		for i := 0; i < N; i++ {
+			v_p[i] = ebigint.NewNBigInt(0).ToRed(b128.Q())
+			v_q[i] = ebigint.NewNBigInt(0).ToRed(b128.Q())
 		}
-		var wPow = ebigint.NewNBigInt(1).ToRed(b128.Q())
-		{
-			for k:=0; k < m; k++ {
-				{//CRnR = CRnR.add(params.getG().mul(phi[k].redNeg().redMul(wPow)));
-					a1 := fq.Neg(phi[k].Int)
-					a2 := fq.Mul(a1, wPow.Int)
-					a3 := b128.G1.MulScalar(this.params.GetG(), a2)
-					CRnR = b128.G1.Add(CRnR, a3)
-				}
-				{//DR = DR.add(params.getG().mul(chi[k].redNeg().redMul(wPow)));
-					a1 := fq.Neg(chi[k].Int)
-					a2 := fq.Mul(a1, wPow.Int)
-					a3 := b128.G1.MulScalar(this.params.GetG(), a2)
-					DR = b128.G1.Add(DR, a3)
-				}
-				{//y_0R = y_0R.add(statement['y'].getVector()[witness['index'][0]].mul(psi[k].redNeg().redMul(wPow)));
-					t_vy := statement["y"].(*GeneratorVector)
-					t_vIndex := witness["index"].([]int)
-					a1 := t_vy.GetVector()[t_vIndex[0]]
-					a2 := fq.Neg(psi[k].Int)
-					a3 := fq.Mul(a2, wPow.Int)
-					a4 := b128.G1.MulScalar(a1, a3)
-					y_0R = b128.G1.Add(y_0R, a4)
-				}
-				{//gR = gR.add(params.getG().mul(psi[k].redNeg().redMul(wPow)));
-					a1 := fq.Neg(psi[k].Int)
-					a2 := fq.Mul(a1, wPow.Int)
-					a3 := b128.G1.MulScalar(this.params.GetG(), a2)
-					gR = b128.G1.Add(gR, a3)
-				}
-				{//y_XR = y_XR.add(proof.y_XG[k].mul(wPow.neg()));
-					a1 := proof.y_XG[k]
-					a2 := wPow.Neg(wPow.Int)
-					a3 := b128.G1.MulScalar(a1, a2)
-					y_XR = b128.G1.Add(y_XR, a3)
-				}
-				p = p.Add(NP[k].Times(wPow))
-				q = q.Add(NQ[k].Times(wPow))
-				wPow = ebigint.ToNBigInt(fq.Mul(wPow.Int, w.Int)).ToRed(wPow.GetRed())
+		p = NewFieldVector(v_p)
+		q = NewFieldVector(v_q)
+	}
+	var wPow = ebigint.NewNBigInt(1).ToRed(b128.Q())
+	{
+		for k := 0; k < m; k++ {
+			{ //CRnR = CRnR.add(params.getG().mul(phi[k].redNeg().redMul(wPow)));
+				a1 := fq.Neg(phi[k].Int)
+				a2 := fq.Mul(a1, wPow.Int)
+				a3 := b128.G1.MulScalar(this.params.GetG(), a2)
+				CRnR = b128.G1.Add(CRnR, a3)
 			}
-			{//CRnR = CRnR.add(statement['CRn'].getVector()[witness['index'][0]].mul(wPow));
-				t_vCRn := statement["CRn"].(*GeneratorVector)
-				t_vIndex := witness["index"].([]int)
-				a1 := t_vCRn.GetVector()[t_vIndex[0]]
-				a2 := b128.G1.MulScalar(a1, wPow.Int)
-				CRnR = b128.G1.Add(CRnR, a2)
+			{ //DR = DR.add(params.getG().mul(chi[k].redNeg().redMul(wPow)));
+				a1 := fq.Neg(chi[k].Int)
+				a2 := fq.Mul(a1, wPow.Int)
+				a3 := b128.G1.MulScalar(this.params.GetG(), a2)
+				DR = b128.G1.Add(DR, a3)
 			}
-			{//y_0R = y_0R.add(statement['y'].getVector()[witness['index'][0]].mul(wPow));
+			{ //y_0R = y_0R.add(statement['y'].getVector()[witness['index'][0]].mul(psi[k].redNeg().redMul(wPow)));
 				t_vy := statement["y"].(*GeneratorVector)
 				t_vIndex := witness["index"].([]int)
 				a1 := t_vy.GetVector()[t_vIndex[0]]
-				a2 := b128.G1.MulScalar(a1,wPow.Int)
-				y_0R = b128.G1.Add(y_0R, a2)
+				a2 := fq.Neg(psi[k].Int)
+				a3 := fq.Mul(a2, wPow.Int)
+				a4 := b128.G1.MulScalar(a1, a3)
+				y_0R = b128.G1.Add(y_0R, a4)
 			}
-			{//DR = DR.add(statement['D'].mul(wPow));
-				t_vD := statement["D"].(utils.Point)
-				a1 := b128.G1.MulScalar(t_vD, wPow.Int)
-				DR = b128.G1.Add(DR,a1)
+			{ //gR = gR.add(params.getG().mul(psi[k].redNeg().redMul(wPow)));
+				a1 := fq.Neg(psi[k].Int)
+				a2 := fq.Mul(a1, wPow.Int)
+				a3 := b128.G1.MulScalar(this.params.GetG(), a2)
+				gR = b128.G1.Add(gR, a3)
 			}
-			{//gR = gR.add(params.getG().mul(wPow));
-				a1 := this.params.GetG()
-				a2 := b128.G1.MulScalar(a1, wPow.Int)
-				gR = b128.G1.Add(gR, a2)
+			{ //y_XR = y_XR.add(proof.y_XG[k].mul(wPow.neg()));
+				a1 := proof.y_XG[k]
+				a2 := wPow.Neg(wPow.Int)
+				a3 := b128.G1.MulScalar(a1, a2)
+				y_XR = b128.G1.Add(y_XR, a3)
 			}
-			{
-				//p = p.add(new FieldVector(Array.from({ length: N }).map((_, i) => i == witness['index'][0] ? wPow : new BN().toRed(bn128.q))));
-				vtp := make([]*ebigint.NBigInt, N)
-				t_vIndex := witness["index"].([]int)
-				for i:=0; i < N; i++ {
-					if i==t_vIndex[0] {
-						vtp[i] = wPow
-					} else {
-						vtp[i] = ebigint.NewNBigInt(0).ToRed(b128.Q())
-					}
-				}
-				tp := NewFieldVector(vtp)
-				p = p.Add(tp)
-				//q = q.add(new FieldVector(Array.from({ length: N }).map((_, i) => i == witness['index'][1] ? wPow : new BN().toRed(bn128.q))));
-				vtq := make([]*ebigint.NBigInt, N)
-				for i:=0; i < N; i++ {
-					if i==t_vIndex[1] {
-						vtq[i] = wPow
-					} else {
-						vtq[i] = ebigint.NewNBigInt(0).ToRed(b128.Q())
-					}
-				}
-				tq := NewFieldVector(vtq)
-				q = q.Add(tq)
-			}
+			p = p.Add(NP[k].Times(wPow))
+			q = q.Add(NQ[k].Times(wPow))
+			wPow = ebigint.ToNBigInt(fq.Mul(wPow.Int, w.Int)).ToRed(wPow.GetRed())
+		}
+		{ //CRnR = CRnR.add(statement['CRn'].getVector()[witness['index'][0]].mul(wPow));
+			t_vCRn := statement["CRn"].(*GeneratorVector)
+			t_vIndex := witness["index"].([]int)
+			a1 := t_vCRn.GetVector()[t_vIndex[0]]
+			a2 := b128.G1.MulScalar(a1, wPow.Int)
+			CRnR = b128.G1.Add(CRnR, a2)
+		}
+		{ //y_0R = y_0R.add(statement['y'].getVector()[witness['index'][0]].mul(wPow));
+			t_vy := statement["y"].(*GeneratorVector)
+			t_vIndex := witness["index"].([]int)
+			a1 := t_vy.GetVector()[t_vIndex[0]]
+			a2 := b128.G1.MulScalar(a1, wPow.Int)
+			y_0R = b128.G1.Add(y_0R, a2)
+		}
+		{ //DR = DR.add(statement['D'].mul(wPow));
+			t_vD := statement["D"].(utils.Point)
+			a1 := b128.G1.MulScalar(t_vD, wPow.Int)
+			DR = b128.G1.Add(DR, a1)
+		}
+		{ //gR = gR.add(params.getG().mul(wPow));
+			a1 := this.params.GetG()
+			a2 := b128.G1.MulScalar(a1, wPow.Int)
+			gR = b128.G1.Add(gR, a2)
 		}
 		{
-			t_vy := statement["y"].(*GeneratorVector)
-			var convolver = NewConvolver()
-			var y_p = convolver.Convolution_Point(p,t_vy)
-			var y_q = convolver.Convolution_Point(q,t_vy)
-			vPow = ebigint.NewNBigInt(1).ToRed(b128.Q())
-			for i:=0; i < N; i++ {
-				var y_poly *GeneratorVector
-				if i%2 != 0 {
-					y_poly = y_q
+			//p = p.add(new FieldVector(Array.from({ length: N }).map((_, i) => i == witness['index'][0] ? wPow : new BN().toRed(bn128.q))));
+			vtp := make([]*ebigint.NBigInt, N)
+			t_vIndex := witness["index"].([]int)
+			for i := 0; i < N; i++ {
+				if i == t_vIndex[0] {
+					vtp[i] = wPow
 				} else {
-					y_poly = y_p
-				}
-				idx := int(math.Floor(float64(i)/2))
-				a1 := y_poly.GetVector()[idx]
-				a2 := b128.G1.MulScalar(a1, vPow.Int)
-				y_XR = b128.G1.Add(y_XR, a2)
-				if i > 0 {
-					vPow = ebigint.ToNBigInt(fq.Mul(vPow.Int,v.Int)).ToRed(vPow.GetRed())
+					vtp[i] = ebigint.NewNBigInt(0).ToRed(b128.Q())
 				}
 			}
+			tp := NewFieldVector(vtp)
+			p = p.Add(tp)
+			//q = q.add(new FieldVector(Array.from({ length: N }).map((_, i) => i == witness['index'][1] ? wPow : new BN().toRed(bn128.q))));
+			vtq := make([]*ebigint.NBigInt, N)
+			for i := 0; i < N; i++ {
+				if i == t_vIndex[1] {
+					vtq[i] = wPow
+				} else {
+					vtq[i] = ebigint.NewNBigInt(0).ToRed(b128.Q())
+				}
+			}
+			tq := NewFieldVector(vtq)
+			q = q.Add(tq)
 		}
-		var k_sk = b128.RanddomScalar()
-		var k_r  = b128.RanddomScalar()
-		var k_b  = b128.RanddomScalar()
-		var k_tau= b128.RanddomScalar()
+	}
+	{
+		t_vy := statement["y"].(*GeneratorVector)
+		var convolver = NewConvolver()
+		var y_p = convolver.Convolution_Point(p, t_vy)
+		var y_q = convolver.Convolution_Point(q, t_vy)
+		vPow = ebigint.NewNBigInt(1).ToRed(b128.Q())
+		for i := 0; i < N; i++ {
+			var y_poly *GeneratorVector
+			if i%2 != 0 {
+				y_poly = y_q
+			} else {
+				y_poly = y_p
+			}
+			idx := int(math.Floor(float64(i) / 2))
+			a1 := y_poly.GetVector()[idx]
+			a2 := b128.G1.MulScalar(a1, vPow.Int)
+			y_XR = b128.G1.Add(y_XR, a2)
+			if i > 0 {
+				vPow = ebigint.ToNBigInt(fq.Mul(vPow.Int, v.Int)).ToRed(vPow.GetRed())
+			}
+		}
+	}
+	var k_sk = b128.RanddomScalar()
+	var k_r = b128.RanddomScalar()
+	var k_b = b128.RanddomScalar()
+	var k_tau = b128.RanddomScalar()
 
-		var A_y = b128.G1.MulScalar(gR, k_sk.Int)
-		var A_D = b128.G1.MulScalar(this.params.GetG(), k_r.Int)
-		var A_b = b128.G1.
+	var A_y = b128.G1.MulScalar(gR, k_sk.Int)
+	var A_D = b128.G1.MulScalar(this.params.GetG(), k_r.Int)
+	var A_b utils.Point
+	{ //var A_b = params.getG().mul(k_b).add(DR.mul(zs[0].redNeg()).add(CRnR.mul(zs[1])).mul(k_sk));
+		a1 := b128.G1.MulScalar(this.params.GetG(), k_b.Int)
 
+		b1 := b128.G1.MulScalar(DR, fq.Neg(zs[0].Int))
+		b2 := b128.G1.MulScalar(CRnR, zs[1].Int)
+		b3 := b128.G1.Add(b1, b2)
+
+		A_b = b128.G1.Add(a1, b3)
+	}
+	var A_X = b128.G1.MulScalar(y_XR, k_r.Int)
+	var A_t utils.Point
+	{
+		a1 := b128.G1.MulScalar(this.params.GetG(), fq.Neg(k_b.Int))
+		a2 := b128.G1.MulScalar(this.params.GetH(), k_tau.Int)
+		A_t = b128.G1.Add(a1, a2)
+	}
+	var A_u utils.Point
+	{ //var A_u = utils.gEpoch(statement['epoch']).mul(k_sk);
+		vepoch := statement["epoch"].(uint)
+		A_u = b128.G1.MulScalar(utils.GEpoch(vepoch), k_sk.Int)
+	}
+	{
+		arguments = abi.Arguments{
+			{
+				Type: bytes32_T,
+			},
+			{
+				Type: bytes32_2T,
+			},
+			{
+				Type: bytes32_2T,
+			},
+			{
+				Type: bytes32_2T,
+			},
+			{
+				Type: bytes32_2T,
+			},
+			{
+				Type: bytes32_2T,
+			},
+			{
+				Type: bytes32_2T,
+			},
+		}
+		x_A_y, y_A_y := b128.Serialize(A_y)
+		x_A_D, y_A_D := b128.Serialize(A_D)
+		x_A_b, y_A_b := b128.Serialize(A_b)
+		x_A_X, y_A_X := b128.Serialize(A_X)
+		x_A_t, y_A_t := b128.Serialize(A_t)
+		x_A_u, y_A_u := b128.Serialize(A_u)
+		bytes, _ = arguments.Pack(
+			b128.Bytes(x.Int),
+			[2]string{x_A_y, y_A_y},
+			[2]string{x_A_D, y_A_D},
+			[2]string{x_A_b, y_A_b},
+			[2]string{x_A_X, y_A_X},
+			[2]string{x_A_t, y_A_t},
+			[2]string{x_A_u, y_A_u},
+		)
+		proof.c = utils.Hash(hex.EncodeToString(bytes))
+	}
+	{
+		t_vsk := witness["sk"].(*ebigint.NBigInt)
+		t_vr := witness["r"].(*ebigint.NBigInt)
+		t_vBTransfer = witness["bTransfer"].(*ebigint.NBigInt)
+		t_vbDiff := witness["bDiff"].(*ebigint.NBigInt)
+
+		red := proof.c.GetRed()
+		a1 := fq.Mul(proof.c.Int, t_vsk.Int)
+		proof.s_sk = ebigint.ToNBigInt(fq.Add(k_sk.Int, a1)).ToRed(red)
+
+		a1 = fq.Mul(proof.c.Int, t_vr.Int)
+		proof.s_r = ebigint.ToNBigInt(fq.Add(k_r.Int, a1)).ToRed(red)
+
+		//proof.s_b = k_b.redAdd(proof.c.redMul(witness['bTransfer'].redMul(zs[0]).redAdd(witness['bDiff'].redMul(zs[1])).redMul(wPow)));
+		b1 := fq.Mul(t_vBTransfer.Int, zs[0].Int)
+		b2 := fq.Mul(t_vbDiff.Int, zs[1].Int)
+		c1 := fq.Add(b1, b2)
+		c2 := fq.Mul(c1, wPow.Int)
+		d1 := fq.Mul(proof.c.Int, c2)
+		proof.s_b = ebigint.ToNBigInt(fq.Add(k_b.Int, d1)).ToRed(red)
+
+		//proof.s_tau = k_tau.redAdd(proof.c.redMul(tauX.redMul(wPow)));
+		m1 := fq.Mul(tauX.Int, wPow.Int)
+		m2 := fq.Mul(proof.c.Int, m1)
+		proof.s_tau = ebigint.ToNBigInt(fq.Add(k_tau.Int, m2)).ToRed(red)
+	}
+	var gs = this.params.GetGS()
+	var hPrimes = this.params.GetHS().Hadamard(ys.Invert())
+	var hExp = ys.Times(z).Add(twoTimesZs)
+	{
+		//var P = proof.BA.add(proof.BS.mul(x)).add(gs.sum().mul(z.redNeg())).add(hPrimes.commit(hExp)); // rename of P
+		a1 := b128.G1.MulScalar(proof.BS, x.Int)
+		a2 := b128.G1.Add(proof.BA, a1)
+
+		b1 := b128.G1.MulScalar(gs.Sum(), fq.Neg(z.Int))
+		b2 := b128.G1.Add(a2, b1)
+
+		c1 := hPrimes.Commit(hExp)
+		c2 := b128.G1.Add(b2, c1)
+		var t_P = c2
+
+		//P = P.add(params.getH().mul(proof.mu.redNeg())); // Statement P of protocol 1. should this be included in the calculation of v...?
+		a1 = b128.G1.MulScalar(this.params.GetH(), fq.Neg(proof.mu.Int))
+		t_P = b128.G1.Add(t_P, a1)
+
+		arguments = abi.Arguments{
+			{
+				Type: bytes32_T,
+			},
+		}
+		bytes, _ = arguments.Pack(
+			b128.Bytes(proof.c.Int),
+		)
+		o := utils.Hash(hex.EncodeToString(bytes))
+
+		//var u_x = params.getG().mul(o); // Begin Protocol 1. this is u^x in Protocol 1. use our g for their u, our o for their x.
+		var u_x = b128.G1.MulScalar(this.params.GetG(), o.Int)
+		//P = P.add(u_x.mul(proof.tHat)); // corresponds to P' in protocol 1.
+		t_P = b128.G1.Add(t_P, b128.G1.MulScalar(u_x, proof.tHat.Int))
+
+		var primeBase = NewGeneratorParams(u_x, gs, hPrimes)
+
+		var ipStatement = make(map[string]interface{})
+		ipStatement["primeBase"] = primeBase
+		ipStatement["P"] = t_P
+		var ipWitness = make(map[string]interface{})
+		ipWitness["l"] = lPoly.Evaluate(x)
+		ipWitness["r"] = rPoly.Evaluate(x)
+
+		proof.ipProof = this.ipProver.GenerateProof(ipStatement, ipWitness, o)
+	}
+	return proof
 }
 
 func PaddingString(in string, padding int) string {
