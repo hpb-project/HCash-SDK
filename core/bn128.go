ecdac69af8a5644a8a29aa5736418b5ca78e6e55
luxq <xueqian1991@gmail.com>
2021-01-04 16:32:14 +0800

    replace wrapper bn128 implement.

--- core/bn128.go
+++ core/bn128.go
@@ -1,84 +1,111 @@
 package core
 
 import (
+	"bytes"
+	"encoding/hex"
 	"github.com/hpb-project/HCash-SDK/common/types"
-	"github.com/hpb-project/HCash-SDK/core/bn128"
+	"github.com/hpb-project/HCash-SDK/core/bn256"
 	"github.com/hpb-project/HCash-SDK/core/ebigint"
+	"log"
 	"math/big"
 )
-
-type BN128 struct {
-	a    string
-	b    string
-	p    *big.Int
-	n    *big.Int
-	gRed bool
-	G1   bn128.G1
-}
-
-type Point [3]*big.Int
-
-func (p Point) Mul(o *ebigint.NBigInt) Point {
-	b128 := NewBN128()
-	return b128.G1.MulScalar(p, o.Int)
-}
-
-func (p Point) Add(o Point) Point {
-	b128 := NewBN128()
-	return b128.G1.Add(p, o)
-}
-func (p Point) Equal(o Point) bool {
-	b128 := NewBN128()
-	return b128.G1.Equal(p, o)
-}
-
-func (p Point) Neg() Point {
-	b128 := NewBN128()
-	return b128.G1.Neg(p)
-}
-
-func NewPoint(bn *BN128, d1, d2 *big.Int) Point {
-	px := ebigint.ToNBigInt(d1).ToRed(bn.CurveRed())
-	py := ebigint.ToNBigInt(d2).ToRed(bn.CurveRed())
-	g := bn128.NewG1(bn.Fq(), [2]*big.Int{px.Int, py.Int})
-	return g.G
-}
 
 var (
 	FIELD_MODULUS, _ = new(big.Int).SetString("30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47", 16)
 	GROUP_MODULUS, _ = new(big.Int).SetString("30644e72e131a029b85045b68181585d2833e84879b9709143e1f593f0000001", 16)
+	bigzero          = "0x0000000000000000000000000000000000000000000000000000000000000000"
 	B_MAX            = 4294967295
 )
 
+type BN128 struct {
+	p  *big.Int
+	n  *big.Int
+	G1 *bn256.G1
+}
+
+type Point struct {
+	p *bn256.G1
+}
+
+func (p Point) Mul(o *ebigint.NBigInt) Point {
+	np := new(bn256.G1).ScalarMult(p.p, o.Int)
+	return Point{np}
+}
+
+func (p Point) Add(o Point) Point {
+	np := new(bn256.G1).Add(p.p, o.p)
+	return Point{np}
+}
+
+func (p Point) Equal(o Point) bool {
+	return p.p.String() == o.p.String()
+}
+
+func (p Point) Neg() Point {
+	np := new(bn256.G1).Neg(p.p)
+	return Point{np}
+}
+
+func (p Point) XY() (*big.Int, *big.Int) {
+	if p.p != nil {
+		x, y, _, _ := p.p.CurvePoints()
+		return x, y
+	} else {
+		return nil, nil
+	}
+}
+
+func NewPoint(d1, d2 *big.Int) Point {
+	x := BytePadding(d1.Bytes(), 32)
+	y := BytePadding(d2.Bytes(), 32)
+	m := BytesCombine(x, y)
+	g, _ := new(bn256.G1).Unmarshal(m)
+	return Point{g}
+}
+
+func BytePadding(data []byte, length int) []byte {
+	datalen := len(data)
+	ret := make([]byte, length)
+	if datalen < length {
+		copy(ret[length-datalen:], data)
+	} else {
+		copy(ret[:], data[datalen-length:])
+	}
+	return ret
+}
+
+func BytesCombine(pBytes ...[]byte) []byte {
+	return bytes.Join(pBytes, []byte(""))
+}
+
 func NewBN128() *BN128 {
 	b := new(BN128)
-	b.a = "0"
-	b.b = "3"
 	b.p = FIELD_MODULUS
 	b.n = GROUP_MODULUS
 
-	b.gRed = false
+	gX, _ := hex.DecodeString("077da99d806abd13c9f15ece5398525119d11e11e9836b2ee7d23f6159ad87d4")
+	gY, _ := hex.DecodeString("01485efa927f2ad41bff567eec88f32fb0a0f706588b4e41a8d587d008b7f875")
+	m := BytesCombine(BytePadding(gX, 32), BytePadding(gY, 32))
+	b.G1, _ = new(bn256.G1).Unmarshal(m)
 
-	gX, _ := big.NewInt(int64(0)).SetString("77da99d806abd13c9f15ece5398525119d11e11e9836b2ee7d23f6159ad87d4", 16)
-	gY, _ := big.NewInt(int64(0)).SetString("1485efa927f2ad41bff567eec88f32fb0a0f706588b4e41a8d587d008b7f875", 16)
-	gG1 := [2]*big.Int{gX, gY}
-
-	//b.fq = bn128.NewFq(b.p) // must use b.p
-	b.G1 = bn128.NewG1(bn128.NewFq(b.p), gG1)
+	if b.G1 == nil {
+		log.Println("new bn128 failed")
+		return nil
+	}
 
 	return b
 }
 
 func (b *BN128) CurveG() Point {
-	return b.G1.G
+	return Point{b.G1}
 }
 
 func (b *BN128) CurveRed() *ebigint.Red {
 	return b.P()
 }
 
-func (b *BN128) Fq() bn128.Fq {
-	return bn128.NewFq(b.p)
+func (b *BN128) Fq() bn256.Fq {
+	return bn256.NewFq(b.p)
 }
 
 func (b *BN128) P() *ebigint.Red {
@@ -90,19 +117,20 @@ func (b *BN128) Q() *ebigint.Red {
 }
 
 func (b *BN128) Zero() Point {
-	data := b.G1.MulScalar(b.G1.G, big.NewInt(0))
-	return Point{data[0], data[1], data[2]}
+	data := new(bn256.G1).ScalarMult(b.G1, big.NewInt(0))
+	return Point{data}
 }
 
 func (b *BN128) RanddomScalar() *ebigint.NBigInt {
-	fq := bn128.NewFq(b.Q().Int)
+	fq := bn256.NewFq(b.Q().Int)
 	r, _ := fq.Rand()
 	nr := ebigint.ToNBigInt(r).ForceRed(b.Q())
 	return nr
 }
 
 func (b *BN128) Bytes(i *big.Int) string {
-	return "0x" + i.Text(16)
+	hexstr := PaddingString(i.Text(16), 64)
+	return "0x" + hexstr
 }
 
 func (b *BN128) B_MAX() int {
@@ -111,12 +139,13 @@ func (b *BN128) B_MAX() int {
 
 func (b *BN128) Serialize(p Point) types.Point {
 	var x, y string
-	if p[0] == nil && p[1] == nil {
-		x = "0x0000000000000000000000000000000000000000000000000000000000000000"
-		y = "0x0000000000000000000000000000000000000000000000000000000000000000"
+	gx, gy := p.XY()
+	if gx == nil || gy == nil {
+		x = bigzero
+		y = bigzero
 	} else {
-		x = b.Bytes(p[0])
-		y = b.Bytes(p[1])
+		x = b.Bytes(gx)
+		y = b.Bytes(gy)
 	}
 	return types.Point{x, y}
 }
@@ -124,25 +153,14 @@ func (b *BN128) Serialize(p Point) types.Point {
 func (b *BN128) UnSerialize(pubkey types.Point) Point {
 	x := pubkey.GX()
 	y := pubkey.GY()
-	if x == "0x0000000000000000000000000000000000000000000000000000000000000000" && y == "0x0000000000000000000000000000000000000000000000000000000000000000" {
+	if x == bigzero && y == bigzero {
 		return b.Zero()
 	} else {
 		d1, _ := big.NewInt(0).SetString(x[2:], 16)
 		d2, _ := big.NewInt(0).SetString(y[2:], 16)
-		return NewPoint(b, d1, d2)
+		return NewPoint(d1, d2)
 	}
 }
-
-//
-//func (b *BN128) UnSerialize(x, y string) Point {
-//	if x == "0000000000000000000000000000000000000000000000000000000000000000" && y == "0000000000000000000000000000000000000000000000000000000000000000" {
-//		return b.Zero()
-//	} else {
-//		d1, _ := big.NewInt(0).SetString(x[2:], 16)
-//		d2, _ := big.NewInt(0).SetString(y[2:], 16)
-//		return NewPoint(b, d1, d2)
-//	}
-//}
 
 func (b *BN128) Representation(p Point) string {
 	key := b.Serialize(p)
--- core/bn128_test.go
+++ core/bn128_test.go
@@ -1,21 +1,61 @@
 package core
 
 import (
+	"encoding/hex"
+	"github.com/hpb-project/HCash-SDK/common"
+	"github.com/hpb-project/HCash-SDK/core/bn256"
+	"github.com/hpb-project/HCash-SDK/core/ebigint"
+	"log"
+	"math/big"
 	"testing"
 )
 
-func TestBase(t *testing.T) {
-	b128 := NewBN128()
+func TestToRed(t *testing.T) {
 	q := b128.Q()
 	if q.Text(16) != "30644e72e131a029b85045b68181585d2833e84879b9709143e1f593f0000001" {
 		t.Error("b128.q is not match")
 	}
 
-	//var secret = "100a1080a8128d4b966bbe15243b9e776db08603f1a36f6d02071fa58d1d5b32"
-	//x := ebigint.FromBytes(common.FromHex(secret)).ToRed(b128.Q())
-	//hexstr := x.Text(16)
-	//log.Println("x = ", hexstr)
-	//if hexstr != "100a1080a8128d4b966bbe15243b9e776db08603f1a36f6d02071fa58d1d5b32" {
-	//	t.Error("ToRed is not match")
-	//}
+	var secret = "0x100a1080a8128d4b966bbe15243b9e776db08603f1a36f6d02071fa58d1d5b32"
+	x := ebigint.FromBytes(common.FromHex(secret[2:])).ToRed(b128.Q())
+	hex_x := b128.Bytes(x.Int)
+	if hex_x != "0x100a1080a8128d4b966bbe15243b9e776db08603f1a36f6d02071fa58d1d5b32" {
+		t.Error("tored is not match")
+	}
+}
+
+func TestKeyPair(t *testing.T) {
+	var secret = "0x100a1080a8128d4b966bbe15243b9e776db08603f1a36f6d02071fa58d1d5b32"
+	x := ebigint.FromBytes(common.FromHex(secret[2:])).ToRed(b128.Q())
+	curve_g := b128.Serialize(b128.CurveG())
+	log.Println("curve g=", curve_g)
+	y := b128.Serialize(b128.CurveG().Mul(x))
+	log.Println("y=", y)
+}
+
+func TestBn256(t *testing.T) {
+	log.Println("in test bn256")
+	gX, _ := hex.DecodeString("077da99d806abd13c9f15ece5398525119d11e11e9836b2ee7d23f6159ad87d4")
+	gY, _ := hex.DecodeString("01485efa927f2ad41bff567eec88f32fb0a0f706588b4e41a8d587d008b7f875")
+
+	m := BytesCombine(BytePadding(gX, 32), BytePadding(gY, 32))
+	log.Println("m= ", hex.EncodeToString(m), "m length = ", len(m))
+
+	g1, ok := new(bn256.G1).Unmarshal(m)
+	if !ok {
+		log.Println("unmarshal failed")
+		//return
+	}
+	log.Println("g1:", g1.String())
+
+	secret, _ := new(big.Int).SetString("100a1080a8128d4b966bbe15243b9e776db08603f1a36f6d02071fa58d1d5b32", 16)
+	pubkey := new(bn256.G1).ScalarMult(g1, secret)
+	log.Println("pubkey:", pubkey.String())
+
+	secret2, _ := new(big.Int).SetString("760a1080a8128d4b966bbe15243b9e776db08603f1a36f6d02071fa58d1d5882", 16)
+	pubkey2 := new(bn256.G1).ScalarMult(g1, secret2)
+	log.Println("pubkey2:", pubkey2.String())
+
+	kadd := new(bn256.G1).Add(pubkey, pubkey2)
+	log.Println("kadd:", kadd.String())
 }
