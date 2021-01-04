c1ff3ed1717338729992217b02e3a6e6d60a47ff
luxq <xueqian1991@gmail.com>
2021-01-04 16:31:35 +0800

    change red.go

--- core/ebigint/red.go
+++ core/ebigint/red.go
@@ -11,21 +11,6 @@ func NewRed(b *big.Int) *Red {
 }
 
 func (this *Red) ConvertTo(b *NBigInt) *NBigInt {
-	var r = this.Mod(this.Int, b.Int)
+	var r = new(big.Int).Mod(b.Int, this.Int)
 	return ToNBigInt(r)
 }
-
-func (this *Red) Number() *big.Int {
-	return this.Int
-}
-
-//func (this *Red) IMod(b *NBigInt) *NBigInt {
-//	return ToNBigInt(b.Mod(b.Int, this.m)).ForceRed(this)
-//}
-
-//func (this *Red) Add(a, b *NBigInt) *NBigInt {
-//	if a.r != nil && a.r == b.r {
-//		a.Add(a.Int,b.Int)
-//	}
-//}
-//
